19afb166c746cafd7488b46804149da75ea567bd
LeFede <lefedeok@gmail.com>
2023-07-12 16:18:25 -0300

    mongoose

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 const express = require("express")
+const mongoose = require("mongoose")
+
 const app = express()
+
 
 app.get("/", (req, res) => {
   res.send("!")
 })
 
-app.listen(4000, () => { console.log('server running')})
+app.listen(4000, () => {
+  (async () => {
+    await mongoose.connect(
+      "mongodb+srv://main:CQBS71F1XJBg4W7L@cluster9.lua1z.mongodb.net/resto?retryWrites=true&w=majority",
+    )
+
+    console.log("Mongoose and express connected! 🚀")
+
+  })()
+})
